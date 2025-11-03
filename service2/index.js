2cce0eca679e38a567e532267459a60747b49a20
Arnav Nikumbh <arnavnikumb@gmail.com>
2025-11-03 08:01:36 +0530

    Add service folders and Dockerfiles

---
+++ service2/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Service 2');
+});
+
+app.listen(3000, () => console.log('Service 2 running on port 3000'));
